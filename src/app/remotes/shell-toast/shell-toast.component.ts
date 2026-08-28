ddf14e4ec625d0e6b92ace125058c08ac535aa3e
prathamMalavi <239937892+prathamMalavi@users.noreply.github.com>
2026-08-28 13:26:41 +0530

    fix: default solution

--- src/app/remotes/shell-toast/shell-toast.component.ts
+++ src/app/remotes/shell-toast/shell-toast.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common'
-import { Component, inject, Input, OnInit } from '@angular/core'
+import { ChangeDetectorRef, Component, inject, Input, NgZone, OnInit, viewChild } from '@angular/core'
 import { UntilDestroy } from '@ngneat/until-destroy'
 import { ReplaySubject } from 'rxjs'
 
@@ -29,13 +29,57 @@ export class OneCXShellToastComponent implements ocxRemoteComponent, ocxRemoteWe
   private readonly primengConfig: PrimeNG = inject(PrimeNG)
   private readonly messageService = inject(MessageService)
   private readonly portalMessageService = inject(PortalMessageService)
+  private readonly ngZone = inject(NgZone)
+  private readonly cdr = inject(ChangeDetectorRef)
+  private readonly toastRef = viewChild('toast', { read: ChangeDetectorRef })
 
   @Input() set ocxRemoteComponentConfig(rcConfig: RemoteComponentConfig) {
     this.ocxInitRemoteComponent(rcConfig)
   }
 
   constructor() {
-    this.portalMessageService.message$.subscribe((message: Message) => this.messageService.add(message))
+    this.solutionDefault()
+    // this.solutionOne()
+    // this.solutionTwo()
+    // this.solutionThree()
+    // this.solutionFour()
+  }
+
+  solutionDefault() {
+    this.portalMessageService.message$.subscribe((message: Message) => {
+      console.log('[Testing Toast] BUG inside Angular zone: Bug: ', NgZone.isInAngularZone(), '| message:', message)
+      this.messageService.add(message)
+    })
+  }
+
+  solutionOne() {
+    this.portalMessageService.message$.subscribe((message: Message) => {
+      console.log('[Testing Toast] SOL1 inside Angular zone: Bug: ', NgZone.isInAngularZone(), '| message:', message)
+      this.ngZone.run(() => this.messageService.add(message))
+    })
+  }
+
+  solutionTwo() {
+    this.portalMessageService.message$.subscribe((message: Message) => {
+      console.log('[Testing Toast] SO2 inside Angular zone: Bug: ', NgZone.isInAngularZone(), '| message:', message)
+      setTimeout(() => this.messageService.add(message))
+    })
+  }
+
+  solutionThree() {
+    this.portalMessageService.message$.subscribe((message: Message) => {
+      console.log('[Testing Toast] SO3 inside Angular zone: Bug: ', NgZone.isInAngularZone(), '| message:', message)
+      this.messageService.add(message)
+      setTimeout(() => this.toastRef()?.detectChanges())
+    })
+  }
+
+  solutionFour() {
+    this.portalMessageService.message$.subscribe((message: Message) => {
+      console.log('[Testing Toast] SO4 inside Angular zone: Bug: ', NgZone.isInAngularZone(), '| message:', message)
+      this.messageService.add(message)
+      setTimeout(() => this.cdr.detectChanges())
+    })
   }
 
   ngOnInit() {
